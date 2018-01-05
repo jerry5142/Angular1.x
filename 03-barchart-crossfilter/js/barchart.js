6330167777b973533186ec6c4897ab9d62909891
Jerry Swank <jerry5142-school@yahoo.com>
2018-01-04 19:03:45 -0600

    Click event workin, but has a bug.
    Chart isn't reset when clicking a bar in a different chart.

--- 03-barchart-crossfilter/js/barchart.js
+++ 03-barchart-crossfilter/js/barchart.js
@@ -2,109 +2,156 @@
 var clickedBar = null;
 
 function init(){
-   //color chart
-   drawBarChart("Colors", "colors", 500, 300, 60, 20, 30, 60, 50, "data/dataCat.json", "category1");
-   //alpha chart
-   drawBarChart("Alphabet", "alphabet", 500, 300, 60, 20, 30, 60, 50, "data/dataCat.json", "category2");
-};
+   var dataFilePath = "data/dataCat.json";
+   
+   var config1 = {
+      id: "colors",
+      title: "Colors",
+      width: 500,
+      height: 300,
+      top: 60, 
+      right: 20, 
+      bottom: 30, 
+      left: 60, 
+      ticIncr: 50,
+      category: "category1"
+   };
 
-//draws a bar chart
-function drawBarChart(title, chartId, chartWidth, chartHeight, marginTop, marginRight, marginBottom, marginLeft, xAxisTicIncr, dataFilePath, category){
-   //create an svg window and append a g element to it
-   var chart = d3.select("#" + chartId).attr("width", chartWidth).attr("height", chartHeight),
-       margin = {top: marginTop, right: marginRight, bottom: marginBottom, left: marginLeft},
-       width = +chart.attr("width") - margin.left - margin.right,
-       height = +chart.attr("height") - margin.top - margin.bottom,
-       g = chart.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        
-   var barHeight = height / 6;
-
-   //set the axis scales
-   var x = d3.scale.linear()
-       .range([0, width]);
-
-   //set the class for the chart 
-   chart.attr("class", "chart");
+   var config2 = {
+      id: "alphabet",
+      title: "Alphabet",
+      width: 500,
+      height: 300,
+      top: 60, 
+      right: 20, 
+      bottom: 30, 
+      left: 60, 
+      ticIncr: 50,
+      category: "category2"
+   };
 
    //get the data and graph it      
    d3.json(dataFilePath, function(data) {
-      //create dimension, group by color, sum by value, and sort by grouped value
+      //crossfilter
       var cf = crossfilter(data.data);
-      var dim = cf.dimension(function(fact){ return fact[category]; });
-      var records = dim.group()
-         .reduceSum(function(d){ return d.value; })
-         .all()
-         .sort(function(x, y){ return d3.ascending(x.value, y.value); });
+      //dimensions
+      var dim1 = cf.dimension(function(fact){ return fact[config1.category]; });
+      var dim2 = cf.dimension(function(fact){ return fact[config2.category]; });
+      //record groups
+      var records1 = groupAndSortDim(dim1);  
+      var records2 = groupAndSortDim(dim2);
+      
+      drawChart(records1, config1, dim1, records2, config2, dim2);     
+      drawChart(records2, config2, dim2, records1, config1, dim1);     
+});
+
+//------------------------------------------------------------------------   
+   function removeChart(chartId){
+      var allGs = d3.selectAll(chartId + " g");
+      allGs.remove();
+   };  
+
+   function drawChart(records, config, dim, linkedRecords, linkedConfig, linkedDim){
+      //create an svg window and append a g element to it
+      var chart = d3.select("#" + config.id).attr("width", config.width).attr("height", config.height),
+          margin = {top: config.top, right: config.right, bottom: config.bottom, left: config.left},
+          width = +chart.attr("width") - config.left - config.right,
+          height = +chart.attr("height") - config.top - config.bottom,
+          g = chart.append("g").attr("transform", "translate(" + config.left + "," + config.top + ")");
             
+      var barHeight = height / 6;
+
+      //set the axis scales
+      var xScaleFunction = d3.scale.linear()
+          .range([0, width]);
+
+      //set the class for the chart 
+      chart.attr("class", "chart");
+
       var maxXtic = d3.max(records, function(d) { return d.value; });
-      if(maxXtic % xAxisTicIncr){ // make max axis tic round up to next xAxisTicIncr
-         maxXtic = maxXtic - (maxXtic % xAxisTicIncr) + xAxisTicIncr;
+      if(maxXtic % config.ticIncr){ // make max axis tic round up to next config.ticIncr
+         maxXtic = maxXtic - (maxXtic % config.ticIncr) + config.ticIncr;
       }
-      var xTicCount = maxXtic / xAxisTicIncr;  
-      x.domain([0, maxXtic]);
+      var xTicCount = maxXtic / config.ticIncr;  
+      xScaleFunction.domain([0, maxXtic]);
       
       var barSection = (height / records.length);
       var barTop = (barSection - barHeight) / 2 ;
-
-      var yAxisLabels = d3.extent(records, function(d) { return d.key; });
+      
       
       var bar = g.selectAll("g")
       .data(records)
       .enter().append("g")
         .attr("transform", function(d, i) { return "translate(0," + Math.round( i * barSection + barTop) + ")"; });
-        
+      
+      //sets the vertical axis labels  
       bar.append("text")
          .attr("class", "chart")
          .attr("x", -10)
          .attr("y", barTop - 5)
          .attr("dy", "0.71em")
-         .attr("text-anchor", "end")
-         .text(function(d){ return d.key; }); 
-     
+       //  .attr("text-anchor", "end")
+//         .text(function(d){ return d.key; }); 
+         .text(function(d){ return d.value; }); 
+      
+      //draws the bars
       bar.append("rect")
-        .attr("width", function(d) { return x(d.value); })
+        .attr("width", function(d) { return xScaleFunction(d.value); })
         .attr("height", barHeight - 1)
- //-----------------------------------       
         .on("click",function(d){
-            var barId = "#" + chartId + " g rect";
+            var barId = "#" + config.id + " g rect";
+            //a bar was clicked so clear all filters
+            dim.filterAll();
+            linkedDim.filterAll();
             if(clickedBar == this){ //No bar is currently selected
                d3.selectAll(barId)
                    .style("fill", "black");            
                clickedBar = null;
-            } else {
+            } else { //deselect all other bars, save the clicked bar, filter and redraw
                d3.selectAll(barId)
                    .style("fill", "gray");
                d3.select(this)           
                   .style("fill", "black");                            
-               clickedBar = this;
-               dim.filter(function(data){ 
-                  if(data === d.key){
-                     return data;
-                  }
-               });
+               clickedBar = this; //save the clicked bar
+               dim.filter(d.key);  //filter the selection
             }
-            records = dim.group()
-         .reduceSum(function(d){ return d.value; });
- /*        .all()
-         .sort(function(x, y){ return d3.ascending(x.value, y.value); });
-   */         console.log(records);
+            /*
+             //redraw charts if the previous click was on a different chart            
+            if(clickedBar && config.id != clickedBar.viewportElement.id){
+               removeChart("#" + config.id); //clear the old chart
+               drawChart(records, config, dim, linkedRecords, linkedConfig, linkedDim); 
+//               removeChart("#" + linkedConfig.id); //clear the old chart
+//               drawChart(linkedRecords, linkedConfig, linkedDim, records, config, dim); 
+           //    clickedBar = null;
+            }            
+           */
+ 
+            
+            removeChart("#" + linkedConfig.id); //clear the old chart
+            drawChart(linkedRecords, linkedConfig, linkedDim, records, config, dim); //redraw
         }); 
- //-----------------------------------
 
       //create the x-axis and move it to the bottom of the graph
       g.append("g")
          .attr("transform", "translate(0," + height + ")")
-         .call(d3.svg.axis().scale(x).orient("bottom").ticks(xTicCount).innerTickSize(-height))
+         .call(d3.svg.axis().scale(xScaleFunction).orient("bottom").ticks(xTicCount).innerTickSize(-height))
          .attr("class", "axis");      
      
       //add a title
       g.append("g")
         .append("text")
-         .attr("class", "chartTitle")     
+         .attr("class", "chartTitle")
          .attr("x", width / 2)             
          .attr("y", -margin.top / 2)
-         .text(title);
-
-   });
+         .text(config.title);
+   };
    
+   function groupAndSortDim(dim){
+      return dim.group()
+         .reduceSum(function(d){ return d.value; })
+         .all()
+      .sort(function(x, y){ return d3.ascending(x.index, y.index); });
+   };
+     
 };
+

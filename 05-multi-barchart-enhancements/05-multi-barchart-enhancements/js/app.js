f535c4943aa940864f61bc58e6d2c81663588345
Jerry Swank <jerry5142-school@yahoo.com>
2018-01-12 15:34:16 -0600

    More refactoring

---
+++ 05-multi-barchart-enhancements/05-multi-barchart-enhancements/js/app.js
@@ -0,0 +1,23 @@
+var app = angular.module("storeApp", ["ngRoute", "ngStorage"]);
+
+app.controller('IndexController', ['$scope', function($scope, $localStorage){
+   $scope.title = 'Gotta Have It!';
+}]);
+
+app.config(function($routeProvider) { 
+   $routeProvider
+   .when("/", {
+     templateUrl : "./html/main.html",
+     controller: "MainController"
+   })
+/*   .when("/shop", {
+     templateUrl : "./html/shop.html",
+     controller: "ShopController"
+   })
+   .when("/cart", {
+     templateUrl : "./html/cart.html",
+     controller: "ShopController"
+   })
+   .otherwise({redirectTo: '/'});
+ */
+});

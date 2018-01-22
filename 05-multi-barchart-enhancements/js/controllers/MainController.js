66165203ca47ac274ec69c1caa0d31369a4e4fb8
Jerry Swank <jerry5142-school@yahoo.com>
2018-01-22 09:07:21 -0600

    Mostly working
    Need tooltip, default button is selected but not shown initially, combine reduce function.

--- 05-multi-barchart-enhancements/js/controllers/MainController.js
+++ 05-multi-barchart-enhancements/js/controllers/MainController.js
@@ -11,15 +11,13 @@ app.controller('MainController', ['$scope', function($scope) {
 
    $scope.resetAll = function(){
       $scope.radioConfigCopy = angular.copy($scope.radioConfig);      
-      resetAllCharts($scope.radioConfigCopy.default);     
+      resetAllCharts($scope.radioConfigCopy.default);
    }
    
    $scope.handleClick = function(value){
       barChartsArray.forEach(function(chart){
          chart.setXaxisValue(value);
-      }); 
-      barChartsArray.forEach(function(chart){
-         chart.scaleXaxis();
+//         chart.scaleXaxis();
          chart.redraw();
       }); 
    };
